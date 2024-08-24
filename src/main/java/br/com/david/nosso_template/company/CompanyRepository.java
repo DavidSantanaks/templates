00b51e3ca88d947cc937d0b2767821f8b3542b1d
davidsantanaks <davidfjobs@gmail.com>
2024-08-23 22:13:06 -0300

    feat: me perdi nos commit mas esta tudo ai

--- src/main/java/br/com/david/nosso_template/company/CompanyRepository.java
+++ src/main/java/br/com/david/nosso_template/company/CompanyRepository.java
@@ -1,4 +1,6 @@
 package br.com.david.nosso_template.company;
 
-public interface CompanyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity,Integer> {
 }
--- src/main/java/br/com/david/nosso_template/templates/templateController.java
+++ src/main/java/br/com/david/nosso_template/templates/templateController.java
@@ -1,5 +1,6 @@
 package br.com.david.nosso_template.templates;
 
+import br.com.david.nosso_template.company.CompanyRepository;
 import br.com.david.nosso_template.pessoas.PessoasEntity;
 import br.com.david.nosso_template.pessoas.dto.PessoaRecord;
 import br.com.david.nosso_template.pessoas.repository.PessoaRepository;
@@ -19,23 +20,27 @@ public class templateController {
     TemplateRepository templateRepository;
     @Autowired
     PessoaRepository pessoaRepository;
+    @Autowired
+    CompanyRepository companyRepository;
 
     @GetMapping()
     public List<TemplateEntity> getAll(){
         return templateRepository.findAll();
     }
 
-    @GetMapping("/{id}")
-    public String findTemplateId(@PathVariable Integer id){
+    @GetMapping("/{id}/pessoa/{idPessoa}")
+    public String findTemplateId(@PathVariable Integer id, @PathVariable Integer idPessoa){
        Optional<TemplateEntity> te = templateRepository.findById(id);
        String templateSave = te.get().getTemplate();
 
-       Optional<PessoasEntity> pessoaRecord = pessoaRepository.findById(id);
-       String corrigida = templateSave.replace("${name}", pessoaRecord.get().getName() );
+       Optional<PessoasEntity> pessoaRecord = pessoaRepository.findById(idPessoa);
+
+       String corrigida = templateSave.replace("${name}", pessoaRecord.get().getName())
+                                       .replace("${company}", pessoaRecord.get().getCompany());
+
+
        templateSave = corrigida;
-
        return templateSave;
-
     }
 
 
@@ -45,8 +50,5 @@ public class templateController {
         templateRepository.save(te);
 
     }
-
-
-
 
 }
