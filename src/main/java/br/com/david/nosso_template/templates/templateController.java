81e2eb03d353a1fbbe7cdfe08c401d72cc43192c
davidsantanaks <davidfjobs@gmail.com>
2024-08-24 00:00:58 -0300

    fix: template controller

--- src/main/java/br/com/david/nosso_template/templates/templateController.java
+++ src/main/java/br/com/david/nosso_template/templates/templateController.java
@@ -2,7 +2,6 @@ package br.com.david.nosso_template.templates;
 
 import br.com.david.nosso_template.company.CompanyRepository;
 import br.com.david.nosso_template.pessoas.PessoasEntity;
-import br.com.david.nosso_template.pessoas.dto.PessoaRecord;
 import br.com.david.nosso_template.pessoas.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +22,7 @@ public class templateController {
     @Autowired
     CompanyRepository companyRepository;
 
+
     @GetMapping()
     public List<TemplateEntity> getAll(){
         return templateRepository.findAll();
@@ -34,10 +34,8 @@ public class templateController {
        String templateSave = te.get().getTemplate();
 
        Optional<PessoasEntity> pessoaRecord = pessoaRepository.findById(idPessoa);
-
        String corrigida = templateSave.replace("${name}", pessoaRecord.get().getName())
                                        .replace("${company}", pessoaRecord.get().getCompany());
-
 
        templateSave = corrigida;
        return templateSave;
