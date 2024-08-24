adf78c0ea805dbb8174f37748e2275bf5b28c8d5
davidsantanaks <davidfjobs@gmail.com>
2024-08-23 22:11:10 -0300

    feat: companyEntity e companyRepository

---
+++ src/main/java/br/com/david/nosso_template/company/CompanyEntity.java
@@ -0,0 +1,22 @@
+package br.com.david.nosso_template.company;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "company")
+@Table(name = "company")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String name;
+
+}
---
+++ src/main/java/br/com/david/nosso_template/company/CompanyRepository.java
@@ -0,0 +1,4 @@
+package br.com.david.nosso_template.company;
+
+public interface CompanyRepository {
+}
