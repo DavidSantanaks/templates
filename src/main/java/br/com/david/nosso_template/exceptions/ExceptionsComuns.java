6bf65edc77a3bd2892b9100cf9bd84c85bf75e6e
David Santana <106870105+DavidSantanaks@users.noreply.github.com>
2024-09-02 21:36:13 -0300

    feat: classe para exceptions comuns

---
+++ src/main/java/br/com/david/nosso_template/exceptions/ExceptionsComuns.java
@@ -0,0 +1,9 @@
+package br.com.david.nosso_template.exceptions;
+
+public class ExceptionsComuns extends RuntimeException{
+
+    public ExceptionsComuns(String mensagem){
+        super(mensagem);
+    }
+
+}
---
+++ src/main/java/br/com/david/nosso_template/parserUtils/ParsesUtils.java
@@ -0,0 +1,4 @@
+package br.com.david.nosso_template.parserUtils;
+
+public class ParsesUtils {
+}
