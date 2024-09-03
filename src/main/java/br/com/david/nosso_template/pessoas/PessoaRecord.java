7049773759b46b9563e35e71758fe3eaa15d3e4f
David Santana <106870105+DavidSantanaks@users.noreply.github.com>
2024-09-02 22:31:19 -0300

    refactor: movida as validacoes para o record

--- src/main/java/br/com/david/nosso_template/pessoas/PessoaRecord.java
+++ src/main/java/br/com/david/nosso_template/pessoas/PessoaRecord.java
@@ -1,12 +1,24 @@
 package br.com.david.nosso_template.pessoas;
 
 import br.com.david.nosso_template.company.CompanyEntity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record PessoaRecord(Integer id,
+                           @NotBlank(message = "Campo nome não pode ser vazio.")
                            String name,
-                           CompanyEntity company,
-                           PessoaEnum type,
-                           String email) {
 
-}
+                           CompanyEntity company,
+
+                           @Enumerated(EnumType.ORDINAL)
+                           @NotNull(message = "Campo tipo não pode ser vazio")
+                           PessoaEnum type,
+
+                           @NotBlank(message = "Email nao pode ser vazio")
+                           @Email(message = "Email tem que ser valido")
+                           String email)
+{ }
