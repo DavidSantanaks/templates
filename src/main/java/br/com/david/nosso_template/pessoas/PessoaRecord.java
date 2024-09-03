204405937af4e0707945c2f0327a47f52d86c013
David Santana <106870105+DavidSantanaks@users.noreply.github.com>
2024-09-02 21:38:08 -0300

    refactor: ordem das informacoes alteradas

--- src/main/java/br/com/david/nosso_template/pessoas/PessoaRecord.java
+++ src/main/java/br/com/david/nosso_template/pessoas/PessoaRecord.java
@@ -3,6 +3,10 @@ package br.com.david.nosso_template.pessoas;
 import br.com.david.nosso_template.company.CompanyEntity;
 
 
-public record PessoaRecord(Integer id, String name, CompanyEntity company, PessoaEnum type, String email) {
+public record PessoaRecord(Integer id,
+                           String name,
+                           CompanyEntity company,
+                           PessoaEnum type,
+                           String email) {
 
 }
