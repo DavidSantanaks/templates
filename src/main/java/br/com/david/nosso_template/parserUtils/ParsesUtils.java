bc890f54799ef3f2e55ce421725172e4cb304a4a
David Santana <106870105+DavidSantanaks@users.noreply.github.com>
2024-09-02 21:37:12 -0300

    feat: classe para criar parses uteis

--- src/main/java/br/com/david/nosso_template/parserUtils/ParsesUtils.java
+++ src/main/java/br/com/david/nosso_template/parserUtils/ParsesUtils.java
@@ -1,4 +1,16 @@
 package br.com.david.nosso_template.parserUtils;
 
+import br.com.david.nosso_template.pessoas.PessoaRecord;
+import br.com.david.nosso_template.pessoas.PessoasEntity;
+
 public class ParsesUtils {
+
+    public static PessoaRecord toPessoaRecord(PessoasEntity pessoas){
+        if (pessoas == null){
+            return null;
+        }
+
+        PessoaRecord convertido = new PessoaRecord(pessoas.getId(), pessoas.getName(), pessoas.getCompany(),pessoas.getType(), pessoas.getEmail());
+        return convertido;
+    }
 }
