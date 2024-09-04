75117684920c3b31861e5074e9f2299163c20d58
David Santana <106870105+DavidSantanaks@users.noreply.github.com>
2024-09-03 22:11:31 -0300

    feat: parses de template

--- src/main/java/br/com/david/nosso_template/parserUtils/ParsesUtils.java
+++ src/main/java/br/com/david/nosso_template/parserUtils/ParsesUtils.java
@@ -2,6 +2,8 @@ package br.com.david.nosso_template.parserUtils;
 
 import br.com.david.nosso_template.pessoas.PessoaRecord;
 import br.com.david.nosso_template.pessoas.PessoasEntity;
+import br.com.david.nosso_template.templates.TemplateEntity;
+import br.com.david.nosso_template.templates.TemplateRecord;
 
 public class ParsesUtils {
 
@@ -20,6 +22,24 @@ public class ParsesUtils {
         }
 
         PessoasEntity convertido = new PessoasEntity(pessoas.id(), pessoas.name(), pessoas.company(),pessoas.type(), pessoas.email());
+        return convertido;
+    }
+
+    public static TemplateRecord toTemplateRecord(TemplateEntity template){
+        if (template == null){
+            return null;
+        }
+
+        TemplateRecord convertido = new TemplateRecord(template.getId(),template.getTemplate(),template.getDateCreate());
+        return convertido;
+    }
+
+    public static TemplateEntity toTemplateEntity(TemplateRecord template){
+        if (template == null){
+            return null;
+        }
+
+        TemplateEntity convertido = new TemplateEntity(template.id(),template.template(),template.dateCreate());
         return convertido;
     }
 }
