7f45517953180056be94aa34d7f9705a1aa785f7
David Santana <106870105+DavidSantanaks@users.noreply.github.com>
2024-09-03 23:01:09 -0300

    refactor: corrigi validador

--- src/main/java/br/com/david/nosso_template/templates/TemplateRecord.java
+++ src/main/java/br/com/david/nosso_template/templates/TemplateRecord.java
@@ -1,6 +1,7 @@
 package br.com.david.nosso_template.templates;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -10,7 +11,7 @@ public record TemplateRecord(
 
 
         Integer id,
-        @NotNull(message = "Template não pode ser nulo")
+        @NotBlank
         String template,
         @CreationTimestamp
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
