68f85f302218194c281a884a8585e6c618312326
David Santana <106870105+DavidSantanaks@users.noreply.github.com>
2024-09-03 22:12:24 -0300

    refactor: validadores para atributos

--- src/main/java/br/com/david/nosso_template/templates/TemplateRecord.java
+++ src/main/java/br/com/david/nosso_template/templates/TemplateRecord.java
@@ -1,6 +1,18 @@
 package br.com.david.nosso_template.templates;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.CreationTimestamp;
+
 import java.time.LocalDateTime;
 
-public record TemplateRecord(Integer id, String template, LocalDateTime dateCreate) {
-}
+public record TemplateRecord(
+
+
+        Integer id,
+        @NotNull(message = "Template não pode ser nulo")
+        String template,
+        @CreationTimestamp
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+        LocalDateTime dateCreate)
+{ }
